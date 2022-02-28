09fe07c3154c869f5ae6041cdb8bd630b9bedc02
Lucas Macedo da Silva <33878052+LuckasMacedo2@users.noreply.github.com>
2022-02-28 20:25:16 -0300

    Update ExemploPostComHttpClient.cs

--- TrechosCodigos/ExemploPostComHttpClient.cs
+++ TrechosCodigos/ExemploPostComHttpClient.cs
@@ -1,5 +1,5 @@
 HttpClient httpClient = new HttpClient();
-string url = "http://localhost:5039/api/v1/produtos/CadastrarProduto";
+string url = "url";
 var objSerializado = JsonSerializer.Serialize(obj);
 var conteudoRequest = new StringContent(objSerializado, Encoding.UTF8, "application/json");
 var resposta = await httpClient.PostAsync(url, conteudoRequest);
